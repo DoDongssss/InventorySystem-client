44a2aa6d45b6dc395a4d35d17177e58e12aae330
DoDongssss <xchitts@gmail.com>
2023-09-23 15:05:00 +0800

    add file

---
+++ src/stores/auth.ts
@@ -0,0 +1,14 @@
+import { defineStore } from "pinia"
+import { ref } from "vue"
+
+export const useAuthStore = defineStore("auth", {
+    state: () => ({
+
+    }),
+    getters: {
+
+    },
+    actions: {
+        
+    }
+})
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="h-screen flex items-center justify-center">
+    <h1>HELLO WORLD!</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
